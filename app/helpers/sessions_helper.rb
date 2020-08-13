3f00c14539901e993942dd0ba8981344d9a4f782
okeysea <ihgboss@gmail.com>
2020-08-13 17:52:39 +0900

    Add redirect_back_or helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,4 +49,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
 end
