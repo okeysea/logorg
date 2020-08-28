28c47208a2c397b5be0ef7e87db723005e4bc63b
okeysea <ihgboss@gmail.com>
2020-08-28 22:52:53 +0900

    Add test for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe Post, type: :model do
       it "is valid" do
         expect(post.valid?).to eq(true)
       end
+      it "is grant post_id" do
+        expect(post.post_id).to_not eq(nil)
+      end
+      it "is duplication post_id does re-grant it" do
+        tmp_post = Post.create params
+        tmp_post.post_id = 'duplication'
+        tmp_post.save
+
+        tmp_post2 = Post.create params
+        tmp_post2.post_id = 'duplication'
+        tmp_post2.save
+
+        tmp_post3 = Post.create params
+        tmp_post3.post_id = 'duplication'
+        tmp_post3.save
+
+        expect(tmp_post2.reload.post_id).to_not eq('deplication')
+        expect(tmp_post3.reload.post_id).to_not eq('deplication')
+        expect(tmp_post2.reload.valid?).to eq(true)
+        expect(tmp_post3.reload.valid?).to eq(true)
+      end
     end
 
     context "invalid user" do
